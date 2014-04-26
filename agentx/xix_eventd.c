a8ffac339f13402eaf90027ca7b91b54d8e9c61d
Markus Juenemann <markus@juenemann.net>
2014-04-26 22:12:00 +1000

    Added command line options

--- agentx/xix_eventd.c
+++ agentx/xix_eventd.c
@@ -1,5 +1,8 @@
 /*
+ * net-snmp sub-agent to easily send xixEventSet and xixEventClear
+ * notifications.
  *
+ * (c) 2014 Markus Juenemann
  *
  * Shamelessly adapted from 
  * http://openhpi.sourceforge.net/subagent-manual/c167.html
@@ -16,6 +19,7 @@
 #include <signal.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <ctype.h>
 
 #include <net-snmp/net-snmp-config.h>
 #include <net-snmp/net-snmp-includes.h>
@@ -23,13 +27,21 @@
 #include <xix_event.h>
 
 #define NAME "xix-eventd"
-#define FIFO "/var/run/xix-event.fifo"
+#define FIFO_PATH "/var/run/xix-event.fifo"
 
         
 static int keep_running;
 
 RETSIGTYPE stop_server(int a) {
     keep_running = 0;
+}
+
+void help() {
+    printf("%s\n", NAME);
+    printf("  -h             print this help\n");
+    printf("  -f <path>      path to FIFO (%s)\n", FIFO_PATH);
+    printf("  -d             enable debug mode\n");
+    exit(0);
 }
 
 
@@ -40,8 +52,37 @@ int main (int argc, char **argv) {
   char *xixEventId, *xixEventText;
   struct stat st;
 
-  /* Change below if you want to be a SNMP master agent */
-  int agentx_subagent=1; /* change this if you want to be a SNMP master agent */
+  int debug = 0;
+  char *fifo_path = FIFO_PATH;
+  int c;
+
+  /* Parse command line */
+  opterr = 0;
+  while ((c = getopt (argc, argv, "hdf:")) != -1) {
+      switch(c) {
+          case 'h':
+              help();
+              break;
+          case 'd':
+              debug = 1;
+              break;
+          case 'f':
+              fifo_path = optarg;
+              break;
+          case '?':
+              if (optopt == 'f')
+                  fprintf (stderr, "option -%c requires an argument.\n", optopt);
+              else if (isprint(optopt)) 
+                  fprintf(stderr, "unknown option '-%c'\n", optopt);
+              else
+                  fprintf (stderr, "Unknown option character `\\x%x'.\n", optopt);
+              help();
+              break;
+          default:
+              abort();
+              
+      }
+  }
 
 
   /* Print log errors to stderr */
@@ -49,16 +90,14 @@ int main (int argc, char **argv) {
 
 
   /* Enable below for debugging */
-  //snmp_set_do_debugging(1);
+  if(debug) {
+      snmp_set_do_debugging(1);
+  }
  
 
-  /* Are we an AgentX subagent? */
-  if (agentx_subagent) {
-
-    /* Make us an AgentX client. */
-    netsnmp_ds_set_boolean(NETSNMP_DS_APPLICATION_ID, 
-                NETSNMP_DS_AGENT_ROLE, 1);
-  }
+  /* Make us an AgentX client. */
+  netsnmp_ds_set_boolean(NETSNMP_DS_APPLICATION_ID, 
+      NETSNMP_DS_AGENT_ROLE, 1);
 
 
   /* Initialize the agent library */
@@ -72,16 +111,12 @@ int main (int argc, char **argv) {
   init_snmp(NAME);
 
 
-  /* Initialised port 161/UDP if we're going to be 
-   * an SNMP master agent */
-  if (!agentx_subagent)
-    init_master_agent();  
-
-
   /* Daemonise */
-  if (netsnmp_daemonize(1, 0)) {
-      snmp_log(LOG_ERR, "failed to become daemon\n");
-      exit(1);
+  if(!debug) {
+      if (netsnmp_daemonize(1, 0)) {
+          snmp_log(LOG_ERR, "failed to become daemon\n");
+          exit(1);
+      }
   }
 
 
@@ -92,17 +127,17 @@ int main (int argc, char **argv) {
 
 
   /* Create the FIFO if it does not exist */
-  if (stat(FIFO, &st)) {
-    if(mknod (FIFO, S_IRUSR|S_IWUSR|S_IFIFO, 0)) {
-      snmp_log(LOG_ERR, "error creating create missing FIFO %s: %s\n", FIFO, strerror(errno));
+  if (stat(fifo_path, &st)) {
+    if(mknod (fifo_path, S_IRUSR|S_IWUSR|S_IFIFO, 0)) {
+      snmp_log(LOG_ERR, "error creating create missing FIFO %s: %s\n", fifo_path, strerror(errno));
       return errno;
     }
   }
 
   
   /* Open FIFO for reading */
-  if ((fp = fopen(FIFO, "r")) == NULL) {
-    snmp_log(LOG_ERR, "error opening FIFO %s: %s\n", FIFO, strerror(errno));
+  if ((fp = fopen(fifo_path, "r")) == NULL) {
+    snmp_log(LOG_ERR, "error opening FIFO %s: %s\n", fifo_path, strerror(errno));
     return errno;
   }
 
@@ -116,9 +151,7 @@ int main (int argc, char **argv) {
      * send off notifications. */
 
     if((fgets(buffer, sizeof(buffer)-1, fp)) == NULL) {
-        //snmp_log(LOG_ERR, "error reading from FIFO %s: %s\n", FIFO, strerror(errno));
         continue;
-        //return errno;
     }
 
     /* Extract xixEventId which is the first word in the line */
